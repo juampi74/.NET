7647b3d5fe5c9e7b132e99835fa498e4d1d32d5a
Juan Pablo Jaca <juampijaca@gmail.com>
2024-04-04 13:11:26 -0300

    Unidad 01 - Capitulo 01 - Laboratorios

---
+++ Capitulo01/Lab01/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Lab01
+{
+    static class Program
+        {
+            static void Main(string[] args) {
+                System.Console.WriteLine("Hola Mundo");
+                Console.ReadLine();
+            }
+        }
+
+}
