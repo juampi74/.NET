b2bad5489ec97ca5d5634de15162cbadc833a2f4
Juan Pablo Jaca <juampijaca@gmail.com>
2024-04-04 13:17:41 -0300

    Unidad 01 - Capitulo 02 - Laboratorio 01 (Proyectos Extra)

---
+++ Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Consola
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Esta linea va a dar error!");
+        }
+    }
+}
+
