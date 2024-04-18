ce69efebcdcf9ba297cf13a8b9becf509c3b7e93
Juan Pablo Jaca <juampijaca@gmail.com>
2024-04-18 13:23:03 -0300

    Unidad 02 - Capitulo 01 - Lab 01 - (Iteracion)

---
+++ Iteracion/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] textos;
+textos = new string[] {"Estamos","en","clase", "de", ".NET"};
+int cantIteraciones = 0;
+
+for (int i = 4; i >= cantIteraciones; i--) {
+    Console.WriteLine(textos[i]);
+}
