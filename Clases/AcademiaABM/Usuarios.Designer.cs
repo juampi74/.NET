0e73f61184ff69849a36dccc5435f65b762b6c9b
Juan Pablo Jaca <juampijaca@gmail.com>
2024-06-06 13:26:52 -0300

    Grilla de Usuarios (Unidad 3 -  Laboratorio 2)

---
+++ Clases/AcademiaABM/Usuarios.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace AcademiaABM
+{
+    partial class Usuarios
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Usuarios));
+            toolStripContainer1 = new ToolStripContainer();
+            tlAlumnos = new TableLayoutPanel();
+            dgvUsuarios = new DataGridView();
+            btnActualizar = new Button();
+            btnSalir = new Button();
+            tsUsuarios = new ToolStrip();
+            tsbNuevo = new ToolStripButton();
+            tsbEditar = new ToolStripButton();
+            tsbEliminar = new ToolStripButton();
+            toolStripContainer1.ContentPanel.SuspendLayout();
+            toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            toolStripContainer1.SuspendLayout();
+            tlAlumnos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvUsuarios).BeginInit();
+            tsUsuarios.SuspendLayout();
+            SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            toolStripContainer1.ContentPanel.Controls.Add(tlAlumnos);
+            toolStripContainer1.ContentPanel.Size = new Size(799, 436);
+            toolStripContainer1.Location = new Point(1, -3);
+            toolStripContainer1.Name = "toolStripContainer1";
+            toolStripContainer1.Size = new Size(799, 461);
+            toolStripContainer1.TabIndex = 0;
+            toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            toolStripContainer1.TopToolStripPanel.Controls.Add(tsUsuarios);
+            // 
+            // tlAlumnos
+            // 
+            tlAlumnos.ColumnCount = 2;
+            tlAlumnos.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tlAlumnos.ColumnStyles.Add(new ColumnStyle());
+            tlAlumnos.Controls.Add(dgvUsuarios, 0, 0);
+            tlAlumnos.Controls.Add(btnActualizar, 0, 1);
+            tlAlumnos.Controls.Add(btnSalir, 1, 1);
+            tlAlumnos.Dock = DockStyle.Fill;
+            tlAlumnos.Location = new Point(0, 0);
+            tlAlumnos.Name = "tlAlumnos";
+            tlAlumnos.RowCount = 2;
+            tlAlumnos.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tlAlumnos.RowStyles.Add(new RowStyle());
+            tlAlumnos.Size = new Size(799, 436);
+            tlAlumnos.TabIndex = 1;
+            // 
+            // dgvUsuarios
+            // 
+            dgvUsuarios.AllowUserToDeleteRows = false;
+            dgvUsuarios.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tlAlumnos.SetColumnSpan(dgvUsuarios, 2);
+            dgvUsuarios.Dock = DockStyle.Fill;
+            dgvUsuarios.Location = new Point(3, 3);
+            dgvUsuarios.Name = "dgvUsuarios";
+            dgvUsuarios.ReadOnly = true;
+            dgvUsuarios.Size = new Size(793, 401);
+            dgvUsuarios.TabIndex = 0;
+            // 
+            // btnActualizar
+            // 
+            btnActualizar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnActualizar.Location = new Point(640, 410);
+            btnActualizar.Name = "btnActualizar";
+            btnActualizar.Size = new Size(75, 23);
+            btnActualizar.TabIndex = 1;
+            btnActualizar.Text = "Actualizar";
+            btnActualizar.UseVisualStyleBackColor = true;
+            btnActualizar.Click += btnActualizar_Click;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(721, 410);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(75, 23);
+            btnSalir.TabIndex = 2;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // tsUsuarios
+            // 
+            tsUsuarios.Dock = DockStyle.None;
+            tsUsuarios.Items.AddRange(new ToolStripItem[] { tsbNuevo, tsbEditar, tsbEliminar });
+            tsUsuarios.Location = new Point(3, 0);
+            tsUsuarios.Name = "tsUsuarios";
+            tsUsuarios.Size = new Size(112, 25);
+            tsUsuarios.TabIndex = 0;
+            tsUsuarios.Text = "toolStrip1";
+            // 
+            // tsbNuevo
+            // 
+            tsbNuevo.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbNuevo.Image = (Image)resources.GetObject("tsbNuevo.Image");
+            tsbNuevo.ImageTransparentColor = Color.Magenta;
+            tsbNuevo.Name = "tsbNuevo";
+            tsbNuevo.Size = new Size(23, 22);
+            tsbNuevo.Text = "toolStripButton1";
+            tsbNuevo.ToolTipText = "Nuevo";
+            // 
+            // tsbEditar
+            // 
+            tsbEditar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbEditar.Image = (Image)resources.GetObject("tsbEditar.Image");
+            tsbEditar.ImageTransparentColor = Color.Magenta;
+            tsbEditar.Name = "tsbEditar";
+            tsbEditar.Size = new Size(23, 22);
+            tsbEditar.Text = "toolStripButton1";
+            tsbEditar.ToolTipText = "Editar";
+            // 
+            // tsbEliminar
+            // 
+            tsbEliminar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbEliminar.Image = (Image)resources.GetObject("tsbEliminar.Image");
+            tsbEliminar.ImageTransparentColor = Color.Magenta;
+            tsbEliminar.Name = "tsbEliminar";
+            tsbEliminar.Size = new Size(23, 22);
+            tsbEliminar.Text = "toolStripButton1";
+            tsbEliminar.ToolTipText = "Eliminar";
+            // 
+            // Usuarios
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(toolStripContainer1);
+            Name = "Usuarios";
+            Text = "Usuarios";
+            toolStripContainer1.ContentPanel.ResumeLayout(false);
+            toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            toolStripContainer1.TopToolStripPanel.PerformLayout();
+            toolStripContainer1.ResumeLayout(false);
+            toolStripContainer1.PerformLayout();
+            tlAlumnos.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvUsuarios).EndInit();
+            tsUsuarios.ResumeLayout(false);
+            tsUsuarios.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ToolStripContainer toolStripContainer1;
+        private ToolStrip tsUsuarios;
+        private TableLayoutPanel tlAlumnos;
+        private DataGridView dgvUsuarios;
+        private Button btnActualizar;
+        private Button btnSalir;
+        private ToolStripButton tsbNuevo;
+        private ToolStripButton tsbEditar;
+        private ToolStripButton tsbEliminar;
+    }
+}
